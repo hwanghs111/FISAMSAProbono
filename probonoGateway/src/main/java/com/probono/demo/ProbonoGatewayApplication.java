8ed07f9801cfb3c80b194ee98fb7e5d0886de163
yeomyaloo <yeom456@gmail.com>
2024-03-06 14:39:50 +0900

    init

---
+++ probonoGateway/src/main/java/com/probono/demo/ProbonoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.probono.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbonoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbonoGatewayApplication.class, args);
+	}
+
+}
---
+++ probonoProject/src/main/java/com/probono/demo/ProbonoUserApplication.java
@@ -0,0 +1,13 @@
+package com.probono.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbonoUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbonoUserApplication.class, args);
+	}
+
+}
