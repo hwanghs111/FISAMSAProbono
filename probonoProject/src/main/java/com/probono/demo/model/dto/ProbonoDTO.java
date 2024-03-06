560422b224376323b3d98bd0281234457bafedc5
yeomyaloo <yeom456@gmail.com>
2024-03-06 19:39:08 +0900

    feat: 정상 작동 ~!

--- probonoProject/src/main/java/com/probono/demo/model/dto/ProbonoDTO.java
+++ probonoProject/src/main/java/com/probono/demo/model/dto/ProbonoDTO.java
@@ -1,5 +1,38 @@
 package com.probono.demo.model.dto;
 
-public class ProbonoDTO {
 
+import com.probono.demo.model.entity.Probono;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+  
+@NoArgsConstructor  
+@AllArgsConstructor 
+@Getter
+@Setter
+@Builder
+public class ProbonoDTO {
+	
+	private String probonoId;
+	private String probonoName;
+	private String probonoPurpose;
+	
+	@Override   
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("프로보노 정보 1. 프로보노 아이디 = ");
+		builder.append(probonoId);
+		builder.append("2. 프로보노 이름 : ");
+		builder.append(probonoName);
+		builder.append("3. 프로보노 목적 : ");
+		builder.append(probonoPurpose);
+		return builder.toString();
+	}
+	
+	public Probono toEntity() {
+		return Probono.builder().probonoId(probonoId).probonoName(probonoName).probonoPurpose(probonoPurpose).build();
+	}
 }
--- probonoProject/src/main/java/com/probono/demo/model/entity/ProbonoProject.java
+++ probonoProject/src/main/java/com/probono/demo/model/entity/ProbonoProject.java
@@ -1,5 +1,31 @@
 package com.probono.demo.model.entity;
 
-public class ProbonoProject {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "probono_project")
+@Entity
+public class ProbonoProject {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int probonoProjectId;
+	private String probonoProjectName;	
+	private String probonoId;
+	private String activistId;
+	private String receiveId; 
+	private String projectContent; 
+	
 }
